faccad7d278ca3be472d78286bee8a7de5759691
btran <btran@LAPTOP-GM8IGRI7>
2023-02-02 18:00:08 -0500

    Add project files.

---
+++ Deliverable4 - Arrays/Program.cs
@@ -0,0 +1,31 @@
+﻿/* 
+ * Author: Tran Nguyen
+ * Date: 2/2/2023
+ * Description: This C# Console application code demonstrates Fibonacci sequence
+ */
+namespace Deliverable4___Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] fibonacci = new int[25] ;
+
+            fibonacci[0] = 0;
+            fibonacci[1] = 1;
+
+            for (int i = 0; i < fibonacci.Length; i++)
+            {
+                if (i >= 2)
+                {
+                    fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+                }
+            }
+            
+            for (int i = 0; i < fibonacci.Length; i++)
+            {
+                Console.WriteLine("Fibonacci(" + i + ") = " + fibonacci[i]);
+            }
+        }
+    }
+}
